5bc7263e200d008555b31b7aece6eb49fbfa5d14
Kenneth Boyles <kenboyles72@suddenlink.net>
2017-08-13 20:41:59 -0500

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,52 @@
+minetest.override_item("default:fence_wood",
+{
+collision_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, -0.5, 0.5, 1.5, 0.5},
+		},
+},
+})
+ 
+minetest.override_item("default:fence_acacia_wood",
+{
+collision_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, -0.5, 0.5, 1.5, 0.5},
+		},
+},
+})
+
+minetest.override_item("default:fence_aspen_wood",
+{
+collision_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, -0.5, 0.5, 1.5, 0.5},
+		},
+},
+})
+
+minetest.override_item("default:fence_junglewood",
+{
+collision_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, -0.5, 0.5, 1.5, 0.5},
+		},
+},
+})
+
+minetest.override_item("default:fence_pine_wood",
+{
+collision_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, -0.5, 0.5, 1.5, 0.5},
+		},
+},
+})
+
+
+
